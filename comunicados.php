4e0e21292c9ab88997667404939a3261a378aa70
Ivan Ruiz <ivanrur95@hotmail.com>
2021-10-20 10:26:06 -0500

    se cambia vista de comunicados

--- comunicados.php
+++ comunicados.php
@@ -95,7 +95,7 @@
 
 
     <div class="row g-5">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <!-- <h3 class="pb-4 mb-4 fst-italic border-bottom">
           Gumen Manos Unidas
           
@@ -104,7 +104,8 @@
 
         <article class="blog-post">
           <h2 class="blog-post-title ">COMUNICADOS</h2>
-
+          
+          <hr>
 
         </article>
 
@@ -210,112 +211,10 @@
 
 
       </div>
-      <div class="col-md-4">
-        <div class="position-sticky" style="top: 2rem;">
-          <div class="p-4 mb-3 bg-light rounded">
-
-          
-            <h4 class="fst-italic">Oficinas</h4>
-
-            <ul class="list-unstyled ps-0">
-              <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                  data-bs-target="#home-collapse" aria-expanded="true">
-                  Cuautitlán Izcalli
-                </button>
-                <div class="collapse show" id="home-collapse">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Empoderamiento de la mujer</a></li>
-                    <li><a href="#" class="link-dark rounded">Conferencia y Talleres</a></li>
-                    <li><a href="#" class="link-dark rounded">Asesoría Jurídica</a></li>
-                    <li><a href="#" class="link-dark rounded">Escuelas</a></li>
-                    <li><a href="#" class="link-dark rounded">Personas con discapacidad</a></li>
-                    <li><a href="#" class="link-dark rounded">Cultura</a></li>
-                    <li><a href="#" class="link-dark rounded">Rescate de espacios públicos</a></li>
-                    <li><a href="#" class="link-dark rounded">Deporte</a></li>
-
-                  </ul>
-                </div>
-              </li>
-              <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                  data-bs-target="#dashboard-collapse" aria-expanded="false">
-                  Tlalnepantla
-                </button>
-                <div class="collapse" id="dashboard-collapse">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Empoderamiento de la mujer</a></li>
-                    <li><a href="#" class="link-dark rounded">Conferencia y Talleres</a></li>
-                    <li><a href="#" class="link-dark rounded">Asesoría Jurídica</a></li>
-                    <li><a href="#" class="link-dark rounded">Escuelas</a></li>
-                    <li><a href="#" class="link-dark rounded">Personas con discapacidad</a></li>
-                    <li><a href="#" class="link-dark rounded">Cultura</a></li>
-                    <li><a href="#" class="link-dark rounded">Rescate de espacios públicos</a></li>
-                    <li><a href="#" class="link-dark rounded">Deporte</a></li>
-                  </ul>
-                </div>
-              </li>
-              <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                  data-bs-target="#orders-collapse" aria-expanded="false">
-                  Tultitlán
-                </button>
-                <div class="collapse" id="orders-collapse">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" class="link-dark rounded">Empoderamiento de la mujer</a></li>
-                    <li><a href="#" class="link-dark rounded">Conferencia y Talleres</a></li>
-                    <li><a href="#" class="link-dark rounded">Asesoría Jurídica</a></li>
-                    <li><a href="#" class="link-dark rounded">Escuelas</a></li>
-                    <li><a href="#" class="link-dark rounded">Personas con discapacidad</a></li>
-                    <li><a href="#" class="link-dark rounded">Cultura</a></li>
-                    <li><a href="#" class="link-dark rounded">Rescate de espacios públicos</a></li>
-                    <li><a href="#" class="link-dark rounded">Deporte</a></li>
-                  </ul>
-                </div>
-              </li>
-              <!-- <li class="border-top my-3"></li> -->
-              <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse"
-                  data-bs-target="#account-collapse" aria-expanded="false">
-                  Toluca
-                </button>
-                <div class="collapse" id="account-collapse">
-                  <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="toluca.php" class="link-dark rounded">Oficina toluca</a></li>
-                    <!-- <li><a href="#" class="link-dark rounded">Profile</a></li>
-                    <li><a href="#" class="link-dark rounded">Settings</a></li>
-                    <li><a href="#" class="link-dark rounded">Sign out</a></li> -->
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div>
-
-
-          <form class="row g-3">
-            <div class="col-md-12">
-              <label for="inputEmail4" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="inputEmail4">
-            </div>
-            <div class="col-md-12">
-              <label for="inputEmail4" class="form-label">Correo electrónico</label>
-              <input type="email" class="form-control" id="inputEmail4">
-            </div>
-            <div class="col-md-12">
-              <label for="inputEmail4" class="form-label">Teléfono</label>
-              <input type="number" class="form-control" id="inputEmail4">
-            </div>
-            <div class="col-12">
-              <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
-
-          </form>
-
-
-
-
-        </div>
-      </div>
+  
+      <!-- TODO: HACER UNA COMPLEMENTO DE COLUMNA  -->
+      <!-- AQUI VA LA COLUMNA DE MENU DESPLEGABLE DE OFICINAS Y FORMULARIO -->
+      
     </div>
 
   </main>
--- dashboardIndex.php
+++ dashboardIndex.php
@@ -8,16 +8,14 @@
 <!doctype html>
 <html lang="en">
   <head>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
     <title>Dashboard Template · Bootstrap v5.1</title>
-
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/dashboard/">
-
-    
 
     <!-- Bootstrap core CSS -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
